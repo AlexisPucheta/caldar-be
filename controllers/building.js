2878035fffc973a9cc5afd0020bb794407e922d5
Franco Di Rosa <fdr.2013@hotmail.com>
2020-11-20 23:56:32 -0300

    Feature branch set-up

---
+++ controllers/building.js
@@ -0,0 +1,16 @@
+/*
+
+Entity fields:
+- _id
+- name: string
+- address: string
+- boilers: array of mongo ids
+- company: mongo id
+
+Controller functions:
+- getBuildingsAll
+- getBuildingById
+- getBuildingsByAttribute
+- deleteBuildingById
+
+*/
