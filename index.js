18ca01be6302c4b884ba6ddd6387b870140506f1
Alexis Pucheta <Alexispucheta22@gmail.com>
2020-11-21 22:08:30 -0300

    GET 'api/technician' added

--- index.js
+++ index.js
@@ -9,3 +9,9 @@ const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
 
 const technicians = require('./data/technician.json');
+
+//technician-controller.getTechniciansAll
+app.get('/api/technician', (req, res) => {
+    res.json(technicians);
+});
+
