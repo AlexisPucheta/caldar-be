e9cf08663a7ed86a8e2d26641502f88b9a5a72b1
Alexis Pucheta <Alexispucheta22@gmail.com>
2020-11-21 22:19:17 -0300

    DELETE 'api/technician/:id' added

--- index.js
+++ index.js
@@ -25,3 +25,12 @@ app.get('/api/technician/:id', (req, res) => {
     }
 });
 
+//technician-controller.deleteTechnicianById
+app.delete('/api/technician/:id', (req, res) => {
+    const found = technicians.some(technician => technician._id.$oid === req.params.id);
+    if (found) {
+        res.json({msg:`Technician with id ${req.params.id} was deleted`, technicians: technicians.filter(technician => technician._id.$oid !== req.params.id)});
+    } else {
+        res.json({msg:`No Technician with id: ${req.params.id}`});
+    }
+});
