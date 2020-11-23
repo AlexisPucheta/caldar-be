a07470bb4207e8d5bad1a5a52ea1b99a4249cb55
Marcos Melgar <marcosjmelgar@gmail.com>
2020-11-23 14:48:25 -0300

    Controllers Added

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
-//console.log('hi');
-
 const express = require('express');
 
 const app = express();
 
 const PORT = process.env.PORT || 4000;
 
+app.use('/companies', require('./controllers/companies'));
+
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+
+
+
+
