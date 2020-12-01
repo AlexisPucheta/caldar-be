c784c45cb193d784f7a873f0c29668add5c2dd62
Franco Di Rosa <fdr.2013@hotmail.com>
2020-11-30 22:00:23 -0300

    Building schema created

---
+++ models/building.js
@@ -0,0 +1,15 @@
+const { ObjectId } = require("mongodb");
+const { model, Schema } = require("mongoose");
+
+const buildingSchema = new Schema({
+    name: {
+        type: String,
+        required: [true, 'Give me a name at least!']
+    },
+    address: String,
+    email: String,
+    boilers: [ObjectId],
+    company: ObjectId
+}, { _id: true, timestamps: true });
+
+module.exports = model('Building', buildingSchema);
