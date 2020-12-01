b154a44af1d7336a7b7984977f1e2093aa33ac45
Franco Di Rosa <fdr.2013@hotmail.com>
2020-12-01 11:45:42 -0300

    Boiler controller and routes updated.

--- controllers/boiler.js
+++ controllers/boiler.js
@@ -1,34 +1,120 @@
-const boilers = require("../data/boiler.json");
+const Boiler = require("../models/boiler.js");
 
-//Get all boilers
+// Create boiler in the database. At least name is required
+exports.createBoiler = (req, res) => {
+    const boiler = new Boiler({
+        name: req.body.name,
+        type: req.body.type
+    });
+    if (boiler.name !== undefined) {
+        boiler
+            .save(boiler)
+            .then(data => {
+                res.status(200).send({
+                    data, 
+                    msg: 'Boiler was succesfully created.'
+                });
+            })
+            .catch(err => {
+                return res.status(500).send({msg: err.message || 'Some error ocurred while creating new boiler.'});
+            });
+    } else {
+        return res.status(400).send({msg: 'Name cannot be empty!'});
+    }
+};
+
+// Retrieve all boilers or get boiler by its attributes from the database.
 exports.getBoilersAll = (req, res) => {
-  switch (Object.keys(req.query)[0]) {
-    case "name":
-      return res.send (boilers.filter(boiler => boiler["name"].includes(req.query.name))); 
-    case "type":
-      return res.send (boilers.filter(boiler => boiler["type"].$oid.includes(req.query.type)));  
-    default: return res.json(boilers);
-  }  
+    const reqQueryObject = req.query;
+    const key = Object.keys(reqQueryObject);
+
+    if (key[0] === undefined) {
+        Boiler.find({})
+        .then(data => {
+            return res.status(200).send(data);
+        })
+        .catch(err => {
+            return res.status(500).send({msg: err.message || 'Some error ocurred while retrieving all boilers.'});
+        });
+    } else {
+        switch (key[0]) {
+            case 'name':
+                Boiler.find({name: reqQueryObject[key]})
+                .then(data => {
+                    if (Object.keys(data).length !== 0) {
+                        return res.status(200).send(data);
+                    } else {
+                        return res.status(404).send({msg: `Doesn't exist any boiler with ${key}: ${reqQueryObject[key]}.`});
+                    }
+                })
+                .catch(err => {
+                    return res.status(500).send({msg: err.message || 'Some error ocurred while retrieving boiler.'});
+                });
+                break;
+            case 'type':
+                Boiler.find({company: reqQueryObject[key]})
+                .then(data => {
+                    if (Object.keys(data).length === 0 ){ //sin el lenght me devuelve vacio si le pongo un ID de type
+                        return res.status(404).send({msg: `Doesn't exist boiler ${key} with id: ${reqQueryObject[key]}.`});
+                    }
+                    res.status(200).send(data);
+                })
+                .catch(err => {
+                    return res.status(500).send({msg: err.message || 'Some error ocurred while retrieving boiler.'});
+                });
+                break;
+            default:
+                return res.status(400).send({msg: `Doesn't exist attribute ${key[0]}.`});
+        }
+    }
 };
 
-//Get boilers by ID
+//Retrieve boiler by id from the database.
 exports.getBoilerById = (req, res) => {
-  const found = boilers.some(boiler => boiler._id.$oid === req.params.id);
-  if (found) {
-    res.json(boilers.filter(boiler => boiler._id.$oid === req.params.id));
-  } else {
-    res.status(400).json({ msg: `No boilers with the id of ${req.params._id}` });
-  }
+    Boiler.findById(req.params.id)
+    .then(data => {
+        if (!data) {
+            return res.status(404).send({msg: `Doesn't exist boiler with id: ${req.params.id}.`});
+        }
+        res.status(200).send(data);
+    })
+    .catch(err => {
+        return res.status(500).send({msg: err.message || 'Some error ocurred while retrieving boiler by id.'});
+    });
 };
 
-//Delete boilers
+// Update boiler by id in the database.
+exports.updateBoilerById = (req, res) => {
+    if (!req.body) {
+        return res.status(400).send({msg: `Data to update cannot be empty!`});
+    }
+    if (!req.body.name || !req.body.type) {
+        return res.status(400).send({msg:`Content cannot be empty!`});
+    }
+
+    Boiler.findOneAndUpdate({_id: req.params.id}, req.body, {useFindAndModify: false})
+        .then(data => {
+            if (!data) {
+                return res.status(404).send({msg:`Boiler with id: ${req.params.id} was not found.`});
+            } else {
+                return res.status(200).send({msg:`Boiler with id: ${req.params.id} was successfully updated.`});
+            }
+        })
+        .catch(err => {
+            return res.status(500).send({msg: err.message || 'Some error ocurred while updating boiler by id.'})
+        })
+};
+
+// Delete boiler by id from the database.
 exports.deleteBoilerById = (req, res) => {
-  const found = boilers.some(boiler => boiler._id.$oid === req.params.id);
-  if (found) {
-    res.json({msg:`Boiler with id ${req.params.id} was deleted`, boilers: boilers.filter(boiler => boiler._id.$oid !== req.params.id)});
-  } else {
-    res
-      .status(400)
-      .json({ msg: `No boilers with the _id of ${req.params._id}` });
-  }
+    Boiler.findOneAndRemove({_id: req.params.id}, {useFindAndModify: false})
+    .then(data => {
+        res.status(200).send({
+            data, 
+            msg: `Boiler with id: ${req.params.id} was succesfully deleted.`
+        });
+    })
+    .catch(err => {
+        return res.status(500).send({msg: err.message || 'Some error ocurred while removing boiler by id.'})
+    });
 };
--- routes/boiler.js
+++ routes/boiler.js
@@ -2,12 +2,16 @@ const boiler = require('../controllers/boiler.js');
 
 var router = require("express").Router();
 
-//Retrieve all boilers
-//getBoilersAll and getBoilersByAttribute
-router.get('/', boiler.getBoilersAll);
-//Retrieve boiler by ID
-router.get('/:id', boiler.getBoilerById);
-//Delete boiler by ID
-router.delete('/:id', boiler.deleteBoilerById);
+// CRUD routes
+// Create boiler in the database.
+router.post('/', building.createBoiler);
+// Retrieve all boilers or get boiler by its attributes from the database.
+router.get('/', building.getBoilersAll);
+// Retrieve boiler by id from the database.
+router.get('/:id', building.getBoilerById);
+// Update boiler by id in the database.
+router.put('/:id', building.updateBoilerById);
+// Delete boiler by id from the database.
+router.delete('/:id', building.deleteBoilerById);
 
 module.exports = router;
