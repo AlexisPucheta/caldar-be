49a166e1d15f0a3e0fddcbb67e33ee6265b9459b
Marcos Melgar <marcosjmelgar@gmail.com>
2020-12-07 20:11:52 -0300

    helper added

---
+++ helpers/company.js
@@ -0,0 +1,24 @@
+const Joi = require("joi");
+Joi.objectId = require("joi-objectid")(Joi);
+
+const companySchema = Joi.object(
+    {
+        CIN: Joi.number().required(),
+
+        name: Joi.string().required(),
+
+        adress: Joi.string().required(),
+
+        zip-code: Joi.string().required(),
+
+        contact: Joi.string().required(),
+
+        email: Joi.string().required(),
+
+        phone: Joi.number().required(),
+
+        buildings: Joi.array().items(Joi.objectId()),
+    }
+);
+
+module.exports = companySchema
