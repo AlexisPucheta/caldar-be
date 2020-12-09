739b32cfccf28ef6284ff6b5d8169d77787b6f92
Franco Di Rosa <fdr.2013@hotmail.com>
2020-12-08 21:30:24 -0300

    Building delete operation tested and working.

--- controllers/building.js
+++ controllers/building.js
@@ -5,25 +5,26 @@ const buildingSchema = require("../helpers/building.js");
 
 // Create building in the database. At least name is required
 exports.createBuilding = async (req, res) => {
-  const building = new Building({
-    company: req.body.company,
-    name: req.body.name,
-    address: req.body.address,
-    zipcode: req.body.zipcode,
-    contact: req.body.contact,
-    phone: req.body.phone,
-    email: req.body.email,
-    obs: req.body.obs,
-    boilers: req.body.boilers,
-  });
-
   try {
     await buildingSchema.validateAsync(req.body);
-    let doesExist;
+
+    const building = new Building({
+      company: req.body.company,
+      name: req.body.name,
+      address: req.body.address,
+      zipcode: req.body.zipcode,
+      contact: req.body.contact,
+      phone: req.body.phone,
+      email: req.body.email,
+      obs: req.body.obs,
+      boilers: req.body.boilers,
+    });
+
+    let doc;
 
     if (building.company !== undefined) {
-      doesExist = await Company.findById(building.company);
-      if (doesExist === null) {
+      doc = await Company.findById(building.company);
+      if (doc === null) {
         return res
           .status(500)
           .send({ msg: `Doesn't exist this company ID: ${building.company}` });
@@ -31,9 +32,9 @@ exports.createBuilding = async (req, res) => {
     }
 
     if (building.boilers !== undefined) {
-      doesExist = await Boiler.find({ _id: building.boilers });
-      if (doesExist.length === building.boilers.length) {
-        doesExist.forEach((boiler) => {
+      doc = await Boiler.find({ _id: building.boilers });
+      if (doc.length === building.boilers.length) {
+        doc.forEach((boiler) => {
           if (boiler.building !== undefined) {
             return res.status(500).send({
               msg: `This boiler id ${boiler.id} already belongs to a building ${boiler.building}`,
@@ -69,7 +70,6 @@ exports.createBuilding = async (req, res) => {
 };
 
 // Retrieve all buildings or get building by its attributes from the database.
-/*
 exports.getBuildingsAll = (req, res) => {
   const key = Object.keys(req.query);
   if (JSON.stringify(req.query) === JSON.stringify({})) {
@@ -101,10 +101,10 @@ exports.getBuildingsAll = (req, res) => {
         });
       });
   }
-}; */
+};
 
 // Retrieve building by id from the database.
-/* exports.getBuildingById = (req, res) => {
+exports.getBuildingById = (req, res) => {
   Building.findById(req.params.id)
     .then((data) => {
       if (!data) {
@@ -123,7 +123,54 @@ exports.getBuildingsAll = (req, res) => {
 };
 
 // Update building by id in the database.
-exports.updateBuildingById = (req, res) => {
+/* exports.updateBuildingById = async (req, res) => {
+  try {
+    await buildingSchema.validateAsync(req.body);
+
+    const boiler = {
+      company: req.body.company,
+      name: req.body.name,
+      address: req.body.address,
+      zipcode: req.body.zipcode,
+      contact: req.body.contact,
+      phone: req.body.phone,
+      email: req.body.email,
+      obs: req.body.obs,
+      boilers: req.body.boilers,
+    };
+
+    let doc = await Building.findById(req.params.id);
+
+    if (req.body.building !== undefined) {
+      const doesExist = await Building.findById(req.body.building);
+      if (doesExist === null) {
+        return res.status(500).send({
+          msg: `Doesn't exist this building ID: ${req.body.building}`,
+        });
+      }
+    }
+
+    await Boiler.findByIdAndUpdate(req.params.id, boiler, {
+      useFindAndModify: false,
+    });
+
+    await Building.findOneAndUpdate(
+      { _id: boiler.building },
+      {
+        $push: {
+          boilers: req.params.id,
+        },
+      },
+      { useFindAndModify: false }
+    );
+
+    doc = await Boiler.findById(req.params.id);
+    return res.send(doc);
+  } catch (err) {
+    return res.send({ msg: `Que paso? ${err}` });
+  }
+}; */
+/* exports.updateBuildingById = (req, res) => {
   if (!req.body) {
     return res.status(400).send({ msg: "Data to update cannot be empty!" });
   }
@@ -159,58 +206,40 @@ exports.updateBuildingById = (req, res) => {
 }; */
 
 // Delete building by id from the database.
-/* exports.deleteBuildingById = (req, res) => {
-  Building.findById(req.params.id)
-    .then(() => {
-      Boiler.deleteMany({ building: { $in: req.params.id } })
-        .then((data) => {
-          console.log("Borro Boilers");
-          res.send({
-            data,
-            msg: "Boilers were succesfully deleted",
-          });
-        })
-        .catch((err) => {
-          return res.status(500).send({
-            msg:
-              err.message ||
-              "Some error ocurred while removing boilers of building.",
-          });
-        });
-    })
-    .then(() => {
-      Building.deleteOne({ _id: req.params.id })
-        .then((data) => {
-          console.log("Borro Building");
-          res.send({
-            data,
-            msg: "Building was succesfully deleted",
-          });
-        })
-        .catch((err) => {
-          return res.status(500).send({
-            msg: err.message || "Some error ocurred while removing building.",
-          });
-        });
-    })
-    .catch((err) => {
-      return res.status(500).send({
-        msg: err.message || "Some error ocurred while removing building by id.",
+exports.deleteBuildingById = async (req, res) => {
+  try {
+    const building = await Building.findById(req.params.id);
+
+    if (building !== null) {
+      if (building.company !== undefined) {
+        await Company.findOneAndUpdate(
+          { _id: building.company },
+          {
+            $pull: {
+              buildings: req.params.id,
+            },
+          },
+          { useFindAndModify: false }
+        );
+      }
+
+      if (building.boilers !== undefined) {
+        await Boiler.deleteMany({ building: req.params.id });
+      }
+
+      await Building.deleteOne({ _id: req.params.id });
+
+      return res.send({
+        building,
+        msg: `Building with ID: ${req.params.id} was successfully deleted.`,
       });
+    }
+    return res
+      .status(404)
+      .send({ msg: `Building with ID: ${req.params.id} was not found.` });
+  } catch (err) {
+    return res.status(500).send({
+      msg: err.message || "Some error ocurred while deleting building by ID.",
     });
-  /* .then((data) => {
-      Building.delete({ _id: { $in: data._in } })
-    }) */
-/* Building.findOneAndRemove({ _id: req.params.id }, { useFindAndModify: false })
-    .then((data) => {
-      res.send({
-        data,
-        msg: `Building with id: ${req.params.id} was succesfully deleted.`,
-      });
-    })
-    .catch((err) => {
-      return res.status(500).send({
-        msg: err.message || "Some error ocurred while removing building by id.",
-      });
-    });
-}; */
+  }
+};
