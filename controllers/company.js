9c48d5d784b191e0faf1e4d61b3db4e91f8d8eca
Marcos Melgar <marcosjmelgar@gmail.com>
2020-12-08 22:45:07 -0300

    delete and update added

--- controllers/company.js
+++ controllers/company.js
@@ -1,5 +1,6 @@
 const Company = require("../models/company.js");
 const companySchema = require("../helpers/company.js");
+const Building = require("../models/building.js");
 const building = require("../models/building.js");
 
 
@@ -110,6 +111,88 @@ exports.getCompanyById = (req, res) => {
         message: err.message || "Some error ocurred while requesting data",
       });
     });
+};
+
+// Update company by id in database
+
+exports.updateCompanyById = async (req, res) => {
+  try {
+    await companySchema.validateAsync(req.body);
+    let doc = await Company.findById(req.params.id);
+    const company = {
+      name: req.body.name,
+      cin: req.body.cin,
+      adress: req.body.adress,
+      zipcode: req.body.zipcode,
+      contact: req.body.contact,
+      email: req.body.email,
+      phone: req.body.phone,
+      building: req.body.building,
+    };
+
+    if (req.body.building !== undefined) {
+      const doesExist = await Building.findById(req.body.building);
+      if (doesExist === null) {
+        return res.status(500).send({
+          msg: `Doesn't exist this building ID: ${req.body.building}`
+        });
+      }
+    }
+
+    await Company.findByIdAndUpdate(req.params.id, company, {
+      useFindAndModify: false,
+    });
+
+    await Building.findOneAndUpdate(
+      {_id: company.building},
+      {
+        $push: {
+          company: req.params.id,
+        },
+      },
+      { useFindAndModify: false}
+    );
+    company.save(company);
+    return res.send(company);
+  } catch (error) {
+    return res.send({msg: `${error.message}`});
+  }
+};
+
+// Delete company by id from database
+
+exports.deleteCompanyById = async (req, res) => {
+  try {
+    const company = await Company.findById(req.params.id);
+
+    if (company !== null) {
+      if (company.building !== undefined) {
+        await building.findOneAndUpdate(
+          { _id: company.building},
+          {
+            $pull: {
+              company: req.params.id,
+            },
+          },
+          { useFindAndModify: false }
+        );
+      }
+
+      await Company.deleteOne({ _id: req.params.id});
+
+      return res.send({
+        company,
+        msg: `Company with id: ${req.params.id}was succesfully deleted`
+      });
+    }
+    return res
+      .status(404)
+      .send ({ msg:`Company with id: ${req.params.id}was not found`});
+  } catch (err) {
+    return res.status(400).send ({
+      msg: err.message || "Some error ocurred while deleting company"
+    });
+  }
 };
 
 
