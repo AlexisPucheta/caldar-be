7f7bdbb38c226518f105308e299efcb92e7e19cc
Alexis Pucheta <Alexispucheta22@gmail.com>
2021-01-07 12:58:34 -0300

    line 24 and 31 fix of company

--- controllers/company.js
+++ controllers/company.js
@@ -21,14 +21,14 @@ exports.createCompany = async (req, res) => {
       obs: req.body.obs,
     });
 
-    const company = await Company.findOne({ name: newCompany.name });
+    let company = await Company.findOne({ name: newCompany.name });
     if (company !== null) {
       return res.status(400).send({
         msg: `This company name: ${newCompany.name} already exist.`,
       });
     }
 
-   const company = await Company.findOne({ CIN: newCompany.CIN });
+   company = await Company.findOne({ CIN: newCompany.CIN });
     if (company !== null) {
       return res.status(400).send({
         msg: `This CIN: ${newCompany.CIN} already exist.`,
